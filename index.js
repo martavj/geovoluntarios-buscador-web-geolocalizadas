3d3b96db62dc837f823f424d2db02f2739b1355c
Raul Jimenez Ortega <rjimenez@esri.com>
2021-07-15 12:34:38 +0200

    Removing unnecesary files for this version

--- index.js
+++ index.js
@@ -1,115 +1,39 @@
 require([
   "esri/config",
-  "esri/Map",
-  "esri/views/MapView",
-  "esri/widgets/Locate",
-  "esri/widgets/BasemapToggle",
-  "esri/layers/FeatureLayer",
   "esri/widgets/Search",
-  "esri/layers/GraphicsLayer",
   "esri/layers/GeoJSONLayer",
-  "esri/Graphic",
-], function (
-  esriConfig,
-  Map,
-  MapView,
-  Locate,
-  BasemapToggle,
-  FeatureLayer,
-  Search,
-  GraphicsLayer,
-  GeoJSONLayer,
-  Graphic
-) {
+  "esri/tasks/Locator",
+], function (esriConfig, Search, GeoJSONLayer, Locator) {
   esriConfig.apiKey =
     "AAPK2ca6b7846e0841c8bcba6dd9cf360db76sr1bqGdjj7ZVHDmq4NhYQke3_PojP3lBlybETdhqNgGAfpz65XQA6YxpnfKGaVr";
 
-  const map = new Map({
-    basemap: "arcgis-navigation",
-    // basemap: "arcgis-topographic", // Basemap layer service
-  });
-
-  const view = new MapView({
-    map: map,
-    center: [-3.600833, 37.178055], // Longitude, latitude
-    zoom: 13, // Zoom level
-    container: "mapDiv", // Div element
-  });
-
-  console.log(view.locate);
-  console.log(view.center.latitude);
-  console.log(view.center.longitude);
-
-  const locationLatitude = null;
-  const locationLongitude = null;
-
-  const locate = new Locate({
-    view: view,
-    useHeadingEnabled: false,
-    goToOverride: function (view, options) {
-      options.target.scale = 1500;
-      console.log(options.target);
-      return view.goTo(options.target);
-    },
-  });
-
-  view.ui.add(locate, "top-left");
-
-  const basemapToggle = new BasemapToggle({
-    view: view,
-    basemap: "arcgis-imagery",
-  });
-
-  view.ui.add(basemapToggle, "bottom-right");
-
   const search = new Search({
-    //Add Search widget
-    view: view,
+    container: document.getElementById("search-box"),
+    sources: [
+      {
+        locator: new Locator({
+          url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+        }),
+        singleLineFieldName: "SingleLine",
+        outFields: ["Addr_type"],
+        name: "ArcGIS World Geocoding Service",
+        placeholder: "Address",
+      },
+    ],
   });
 
-  view.ui.add(search, "top-right");
+  let database;
 
-  // const graphicsLayer = new GraphicsLayer();
-  // map.add(graphicsLayer);
-
-  // const point = {
-  //   type: "point",
-  //   longitude: view.center.longitude,
-  //   latitude: view.center.latitude,
-  // };
-
-  // const simpleMarkerSymbol = {
-  //   type: "simple-marker",
-  //   color: [226, 119, 40],
-  //   outline: {
-  //     color: [255, 255, 255],
-  //     width: 1,
-  //   },
-  // };
-
-  // const pointGraphic = new Graphic({
-  //   geometry: point,
-  //   symbol: simpleMarkerSymbol,
-  // });
-
-  // graphicsLayer.add(pointGraphic);
-
-  const geoJSONLayer = new GeoJSONLayer({
+  const layer = new GeoJSONLayer({
     url: "data/test.json",
   });
-  geoJSONLayer.when((l) => {
-    debugger;
-    geoJSONLayer // queries all features in the layer
-      .queryFeatures((f) => {
-        debugger;
-      }); // queries features and returns a FeatureSet
-    // .queryExtent() // queries features returns extent of features that satisfy query
-    // .queryFeatureCount() // queries features and returns count of features
-    // .queryObjectIds(); // queries features and returns objectIds array of features
+
+  layer.queryFeatures().then(function (results) {
+    console.log(results.features);
+    database = results.features;
   });
 
   /*
-    1. Cargar geojson (queryFeatures o fetch?)
     2. Mostrar caja de búsqueda
       2.1 Cuando se obtenga localización (location / extent)
       2.2 Intersect ubicación usando GeometryEngine.inserct(geometriasGeoJSON, localizacion)
