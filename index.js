236b46de0365f8335d0b4275d05764f26d779d46
Villalobos Marta <marta.villalobos.jimenez@gmail.com>
2021-10-16 15:59:49 +0100

    update data file

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ require([
   "esri/config",
   "esri/widgets/Search",
   "esri/layers/GeoJSONLayer",
-], function (esriConfig, Search, GeoJSONLayer) {
-  esriConfig.apiKey = apiKey;
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/GraphicsLayer",
+  "esri/Graphic",
+], function (
+  esriConfig,
+  Search,
+  GeoJSONLayer,
+  Map,
+  MapView,
+  GraphicsLayer,
+  Graphic
+) {
+  //esriConfig.apiKey = apiKey;
+  esriConfig.apiKey =
+    "AAPK87580e590af24ff4a112832c43dbb704_CtPugq6Hs7TGTKzl-hgwybVFmduOYi9arnEQy3D2VJdsZz0Qns4n6tlJRIhtAcE";
 
   // Search widget which includes the locator. We will get the location from the results.
   const search = new Search({
@@ -14,9 +28,6 @@ require([
     source.placeholder = "Introduce tu dirección";
     watcher.remove();
   });
-
-  // Use the event select results to get the coordinates
-  // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Search.html#event-select-result
 
   const database = new GeoJSONLayer({
     url: "data/data.json",
@@ -32,51 +43,15 @@ require([
         outFields: ["*"],
       })
       .then((res) => {
+        const locationLatitude = event.result.feature.geometry.latitude;
+        const locationLongitude = event.result.feature.geometry.longitude;
+
         if (res.features.length > 0) {
           resultsBoxEl.innerHTML = "";
-
-          res.features.forEach((el) => {
-            const searchPanel = document.getElementById("search-panel");
-            searchPanel.classList.remove("h-screen");
-            searchPanel.classList.add("mt-10");
-
-            const listEl = document.createElement("div");
-            listEl.className = "listEl sm:flex p-2 my-5";
-            const noLogo = "geosearch.png";
-
-            listEl.innerHTML = `
-              <div class="w-32 p-4">
-                <img src=${
-                  el.attributes.logo ? el.attributes.logo : noLogo
-                }></img>
-              </div>
-              <div class="ml-5">
-                <h1 class="name">${el.attributes.name}</h1>
-                <ul class="features">
-                  <li><strong>Riesgo: </strong>${
-                    el.attributes.riesgo ? el.attributes.riesgo : ""
-                  }</li>
-                  <li><strong>Información: </strong>${
-                    el.attributes.info ? el.attributes.info : ""
-                  }</li>
-                  <li><strong>Ámbito territorial: </strong>${
-                    el.attributes.territorio ? el.attributes.territorio : ""
-                  }</li>
-                  <li><strong>Idiomas: </strong>${
-                    el.attributes.idiomas ? el.attributes.idiomas : ""
-                  }</li>
-                  <li><strong>Descripción: </strong>${
-                    el.attributes.descripcion ? el.attributes.descripcion : ""
-                  }</li>
-                </ul>
-                <a class="url" href="${el.attributes.url}" target="_blank">${
-              el.attributes.url
-            }</a>
-              </div>
-            `;
-
-            resultsBoxEl.appendChild(listEl);
-          });
+          //Add map
+          showMap(locationLongitude, locationLatitude);
+          //Get info list
+          getList(res, locationLatitude, locationLongitude);
         } else {
           resultsBoxEl.innerHTML = "<p>No hay resultados</p>";
         }
@@ -87,4 +62,88 @@ require([
         console.error("query failed: ", e);
       });
   });
+
+  //Get list of results
+  getList = (res, locationLongitude, locationLatitude) => {
+    console.log("Getting list...");
+    res.features.forEach((el) => {
+      const searchPanel = document.getElementById("search-panel");
+      searchPanel.classList.remove("h-screen");
+      searchPanel.classList.add("mt-10");
+
+      const listEl = document.createElement("div");
+      listEl.className = "listEl sm:flex p-2 my-5";
+      const noLogo = "geosearch.png";
+      listEl.innerHTML = `
+        <div class="w-32 p-4">
+          <img src=${el.attributes.logo ? el.attributes.logo : noLogo}></img>
+        </div>
+        <div class="ml-5">
+          <h1 class="name">${el.attributes.name}</h1>
+          <ul class="features">
+            <li><strong>Riesgo: </strong>${
+              el.attributes.hazardType ? el.attributes.hazardType : ""
+            }</li>
+            <li><strong>Información: </strong>${
+              el.attributes.infoCategory ? el.attributes.infoCategory : ""
+            }</li>
+            <li><strong>Ámbito territorial: </strong>${
+              el.attributes.territorialScope
+                ? el.attributes.territorialScope
+                : ""
+            }</li>
+            <li><strong>Idiomas: </strong>${
+              el.attributes.languages ? el.attributes.languages : ""
+            }</li>
+            <li><strong>Descripción: </strong>${
+              el.attributes.description ? el.attributes.description : ""
+            }</li>
+          </ul>
+          <a class="url" href="${el.attributes.url}" target="_blank">${
+        el.attributes.url
+      }</a>
+        </div>
+      `;
+
+      resultsBoxEl.appendChild(listEl);
+    });
+  };
+
+  showMap = (locationLongitude, locationLatitude) => {
+    const map = new Map({
+      basemap: "arcgis-navigation",
+    });
+
+    const view = new MapView({
+      map: map,
+      container: "map",
+      center: [locationLongitude, locationLatitude],
+      zoom: 16,
+    });
+
+    const graphicsLayer = new GraphicsLayer();
+    map.add(graphicsLayer);
+
+    const point = {
+      type: "point",
+      longitude: locationLongitude,
+      latitude: locationLatitude,
+    };
+
+    const simpleMarkerSymbol = {
+      type: "simple-marker",
+      color: [226, 119, 40],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    };
+
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: simpleMarkerSymbol,
+    });
+
+    graphicsLayer.add(pointGraphic);
+  };
 });
